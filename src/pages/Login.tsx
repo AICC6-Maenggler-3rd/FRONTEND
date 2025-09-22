5ad948ee660e8eb5fcaa52d233b963bfbeb2772d
Wasa-B <ghd1876@gmail.com>
2025-09-22 15:46:13 +0900

    네이버 로그인 테스트버튼

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import GoogleLogin from '../components/common/GoogleLogin'
+import NaverLogin from '../components/common/NaverLogin'
 
 const Login = () => {
   return (
     <div>
       <GoogleLogin/>
+      <NaverLogin/>
     </div>
   )
 }
