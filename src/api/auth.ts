bbe14cb66860520a848d64a97a94f6226db2c96f
Wasa-B <ghd1876@gmail.com>
2025-09-30 15:02:48 +0900

    로그인 api 요청 코드 분리

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,11 +1,28 @@
 import axios from "axios";
 
-const API_BASE = "http://localhost:8000"; // FastAPI 주소
+const API_BASE = import.meta.env.VITE_API_HOST ||"http://localhost:8000"; // FastAPI 주소
+
+const loginRedirect = async (provider:string)=>{
+  try {
+    const res = await axios.get(`${API_BASE}/auth/${provider}/login`);
+    window.location.href = res.data.auth_url;
+  } catch (error) {
+    console.log(`${provider} 로그인 요청 실패`)
+  }
+}
 
 export const googleLoginUrl = async () => {
-  const res = await axios.get(`${API_BASE}/auth/google/login`);
-  return res.data; // 구글 OAuth redirect URL
+  await loginRedirect("google");
 };
+
+export const naverLoginUrl = async () => {
+  await loginRedirect("naver");
+};
+
+export const kakaoLoginUrl = async () => {
+  await loginRedirect("kakao");
+};
+
 
 export const getUserInfo = async () => {
   const res = await axios.get(`${API_BASE}/auth/user`, { withCredentials: true });
--- src/components/common/NaverLogin.tsx
+++ src/components/common/NaverLogin.tsx
@@ -1,26 +1,10 @@
 import React from 'react'
 import axios from "axios";
+import { naverLoginUrl } from '../../api/auth';
 const NaverLogin = () => {
-  const handleNaverLogin = async () => {
-    try {
-      // 1. 백엔드에서 auth_url 받아오기
-      const res = await axios.get("http://localhost:8000/auth/naver/login", {
-        withCredentials: true,
-      });
-
-      const authUrl = res.data.auth_url;
-      if (authUrl) {
-        // 2. 네이버 로그인 페이지로 이동
-        window.location.href = authUrl;
-      }
-    } catch (error) {
-      console.error("네이버 로그인 요청 실패:", error);
-    }
-  };
-
   return (
     <button
-      onClick={handleNaverLogin}
+      onClick={naverLoginUrl}
       className="px-4 py-2 bg-green-600 text-white rounded-lg hover:bg-green-700"
     >
       네이버 로그인
