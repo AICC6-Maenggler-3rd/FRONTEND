f0b506aab01d368c58d6b439456de06adceb7486
김준영 <1090jun@naver.com>
2025-10-01 13:41:06 +0900

    app.tsx 구조 변경

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,36 +1,41 @@
-import axios from "axios";
+import axios from 'axios';
 
-const API_BASE = import.meta.env.VITE_API_HOST ||"http://localhost:8000"; // FastAPI 주소
+const API_BASE = import.meta.env.VITE_API_HOST || 'http://localhost:8000'; // FastAPI 주소
 
-const loginRedirect = async (provider:string)=>{
+const loginRedirect = async (provider: string) => {
   try {
     const res = await axios.get(`${API_BASE}/auth/${provider}/login`);
     window.location.href = res.data.auth_url;
   } catch (error) {
-    console.log(`${provider} 로그인 요청 실패`)
+    console.log(`${provider} 로그인 요청 실패`);
   }
-}
+};
 
 export const googleLoginUrl = async () => {
-  await loginRedirect("google");
+  await loginRedirect('google');
 };
 
 export const naverLoginUrl = async () => {
-  await loginRedirect("naver");
+  await loginRedirect('naver');
 };
 
 export const kakaoLoginUrl = async () => {
-  await loginRedirect("kakao");
+  await loginRedirect('kakao');
 };
 
-
 export const getUserInfo = async () => {
-  const res = await axios.get(`${API_BASE}/auth/user`, { withCredentials: true });
-  console.log(res)
+  const res = await axios.get(`${API_BASE}/auth/user`, {
+    withCredentials: true,
+  });
+  console.log(res);
   return res.data;
 };
 
 export const logout = async () => {
-  const res = await axios.post(`${API_BASE}/auth/logout`, {}, { withCredentials: true });
+  const res = await axios.post(
+    `${API_BASE}/auth/logout`,
+    {},
+    { withCredentials: true },
+  );
   return res.data;
 };
