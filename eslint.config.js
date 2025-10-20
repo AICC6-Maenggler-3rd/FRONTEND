ece366ad1a65b0342316bcdfa4b95a2169788cf9
Wasa-B <ghd1876@gmail.com>
2025-10-20 14:30:23 +0900

    lint 설정 수정

--- eslint.config.js
+++ eslint.config.js
@@ -21,6 +21,8 @@ export default tseslint.config([
     },
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
+      "@typescript-eslint/no-empty-object-type": "off",
+      "@typescript-eslint/no-unused-vars": "off",
     },
   },
 ]);
