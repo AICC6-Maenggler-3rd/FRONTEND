797715077c24bdcfae42917ebe0be418b0bce911
Wasa-B <ghd1876@gmail.com>
2025-10-20 14:38:01 +0900

    build시 검사 수정

--- eslint.config.js
+++ eslint.config.js
@@ -21,8 +21,6 @@ export default tseslint.config([
     },
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
-      "@typescript-eslint/no-empty-object-type": "off",
-      "@typescript-eslint/no-unused-vars": "off",
     },
   },
 ]);
